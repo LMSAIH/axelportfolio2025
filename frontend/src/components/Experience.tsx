a4a099b3bfd99e42f769fdedd380338a9bb11430
Axel Velasquez <yoloax11@gmail.com>
2025-06-10 11:08:32 -0700

    changes

--- frontend/src/components/Experience.tsx
+++ frontend/src/components/Experience.tsx
@@ -14,6 +14,7 @@ interface WorkExperience {
 
 const Experience: React.FC = () => {
     
+
     // Sample work experience data - replace with your own
     const experiences: WorkExperience[] = [
           {
